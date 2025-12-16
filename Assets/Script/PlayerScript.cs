d49b98ec098e7a016b0e2d9df75d6378a2645dc5
kutsunaharuki <ca01254@st.kawahara.ac.jp>
2025-12-16 14:51:27 +0900

    Playerの移動を実装
    簡単な移動を追加

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    //プレイヤーの移動。
+    private void PlayerMove()
+    {
+        //移動キー。
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //移動量の計算。
+        Vector3 move = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+
+        //位置の更新。
+        transform.position += move;
+    }
+}
