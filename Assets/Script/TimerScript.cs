74d1a10f95fc27495fc0612bc65c0734876d8c2b
unknown <sennbatomohiro@gmail.com>
2025-12-17 18:40:53 +0900

    制限時間を追加

---
+++ Assets/Script/TimerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TimerScript : MonoBehaviour
+{
+
+    private float timeLimit = 90.0f;
+    public TextMeshProUGUI timerText;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if(timeLimit > 0)
+        {
+            timeLimit -= Time.deltaTime;
+            UpdateTimerText(timeLimit);
+        }
+        else
+        {
+            timeLimit = 0;
+            UpdateTimerText(timeLimit);
+            TimeUp();
+        }
+    }
+
+
+    void UpdateTimerText(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+    void TimeUp()
+    {
+        Debug.Log("TimeUp!");
+    }
+}
