9588a9dba7a625e31b881ccd143b5183be9a88bc
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2025-12-19 13:45:21 +0900

    ResultSceneの用意とボタンを実装をしました。

--- Assets/Script/OutGame/TitleSceneTransitionScript.cs
+++ Assets/Script/OutGame/TitleSceneTransitionScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartButtonScript : MonoBehaviour
+public class TitleSceneTransitionScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class StartButtonScript : MonoBehaviour
 
     public void OnClick()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("InGameScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
     }
 }
