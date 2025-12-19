473103fa524251cfea5e69b4ac89dc7f0edc51f8
TakumuIseki <t-iseki@kawahara.ac.jp>
2025-12-19 19:00:49 +0900

    ナビメッシュ調整

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+///
 public class EnemyScript : MonoBehaviour
 {
-    [SerializeField]GameObject Player;
+    [SerializeField] GameObject Player;
     PlayerScript playerScript;
     Vector2 PlayerPos;
 
@@ -14,6 +13,10 @@ public class EnemyScript : MonoBehaviour
     Vector3 vector;
 
     private Rigidbody2D rb;
+
+    [SerializeField]
+    NavMeshAgent2D agent;
+    [SerializeField] Transform target;
 
     private void Start()
     {
@@ -30,30 +33,30 @@ public class EnemyScript : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-   
+
     void FixedUpdate()
     {
-        //プレイヤーの現在の位置を取得
-        PlayerPos = playerScript.GetPosition();
-        //現在位置からプレイヤ－の位置に向けて移動
-        //transform.position = Vector2.MoveTowards(transform.position, PlayerPos, speed * Time.deltaTime);
-        Vector2 nextPos= Vector2.MoveTowards(rb.position, PlayerPos, speed * Time.fixedDeltaTime);
-        rb.MovePosition(nextPos);
-        //プレイヤーとエネミーのX軸の位置関係を取得する
-        diff.x = PlayerPos.x - rb.position.x;
-        //プレイヤーがエネミーの右側にいるとき右を向く
-        if(diff.x >0)
-        {
-            //vector = new Vector3(0, -180, 0);
-            //transform.eulerAngles = vector;
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        //Playerが敵キャラの左側にいるとき左側を向く
-        if(diff.x < 0)
-        {
-            //vector = new Vector3(0, 0, 0);
-            //transform.eulerAngles = vector;
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+        agent.SetDestination(target.position); //こっちの書き方でもオッケー
+
+        ////プレイヤーの現在の位置を取得
+        //PlayerPos = playerScript.GetPosition();
+        ////現在位置からプレイヤ－の位置に向けて移動
+        ////transform.position = Vector2.MoveTowards(transform.position, PlayerPos, speed * Time.deltaTime);
+        //Vector2 nextPos = Vector2.MoveTowards(rb.position,PlayerPos,speed * Time.fixedDeltaTime);
+        //rb.MovePosition(nextPos);
+        ////プレイヤーとエネミーのX軸の位置関係を取得する
+        //diff.x = PlayerPos.x - rb.position.x;
+        ////プレイヤーがエネミーの右側にいるとき右を向く
+        //if(diff.x > 0)
+        //{
+        //    vector = new Vector3(0,-180,0);
+        //    transform.eulerAngles = vector;
+        //}
+        ////Playerが敵キャラの左側にいるとき左側を向く
+        //if(diff.x < 0)
+        //{
+        //    vector = new Vector3(0,0,0);
+        //    transform.eulerAngles = vector;
+        //}
     }
 }
