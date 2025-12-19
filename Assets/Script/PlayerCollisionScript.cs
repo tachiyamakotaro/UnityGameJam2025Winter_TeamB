eab2289c10458beceb167434a05b3bd4030669ed
kutsunaharuki <ca01254007@st.kawahara.ac.jp>
2025-12-19 14:37:05 +0900

    Kutsuna/player (#23)
    * Playerの移動を実装
    
    簡単な移動を追加
    
    * PlayerをPrefab化の修正
    
    * Playerに追従するカメラを追加
    
    Playerの動きに合わせて動くカメラ
    GetPositionを追加
    
    * カメラの保存
    
    * Playerの修正
    
    * Playerの挙動とカメラの追従の修正
    
    Playerを再作成しPrefabの不具合を修正
    
    * 修正
    
    * PlayerのSprite画像の差し替え
    
    * HPバーの追加
    
    Playerの足元に追従するHPバー
    一応のダメージ処理
    PlayerStateのステータス管理元を作成
    PlayerCollisionを作成
    
    ---------
    
    Co-authored-by: kutsunaharuki <ca01254@st.kawahara.ac.jp>
    Co-authored-by: SennbaTomohiro <sennbatomohiro@gmail.com>

---
+++ Assets/Script/PlayerCollisionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionScript : MonoBehaviour
+{
+    private PlayerStateScript playerState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerState = GetComponent<PlayerStateScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            playerState.TakeDamage(5);
+        }
+    }
+}
--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,6 +1,5 @@
 ﻿
 using System.Collections;
-
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,27 +7,23 @@ public class PlayerScript : MonoBehaviour
 {
     private float speed = 5.0f;
 
-    private Rigidbody2D rb;
-    private Vector2 movement;
+
+    [SerializeField] HpBarScript hpBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Move();
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement.Normalize();
+
+        Move();
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-    }
 
     private void Move()
     {
