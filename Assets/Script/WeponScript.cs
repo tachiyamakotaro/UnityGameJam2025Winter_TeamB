303105c22c1bfd8257a994f43c58b2fbfbfd2eca
kutsunaharuki <ca01254007@st.kawahara.ac.jp>
2025-12-21 16:09:21 +0900

    Kutsuna/wepons (#29)
    * Playerの移動を実装
    
    簡単な移動を追加
    
    * PlayerをPrefab化の修正
    
    * Playerに追従するカメラを追加
    
    Playerの動きに合わせて動くカメラ
    GetPositionを追加
    
    * カメラの保存
    
    * Playerの修正
    
    * Playerの挙動とカメラの追従の修正
    
    Playerを再作成しPrefabの不具合を修正
    
    * 修正
    
    * PlayerのSprite画像の差し替え
    
    * HPバーの追加
    
    Playerの足元に追従するHPバー
    一応のダメージ処理
    PlayerStateのステータス管理元を作成
    PlayerCollisionを作成
    
    * 武器の挙動と攻撃方法を追加
    
    TagがEnemyの時のダメージ処理は「Enemy担当」の方
    武器を投げるとPlayerの元に戻ってくるシステムを導入
    基底クラスのWeponScriptsの追加
    派生クラスに武器の共通処理を追加
    武器のPrefabにCollider2Dを追加
    
    ---------
    
    Co-authored-by: kutsunaharuki <ca01254@st.kawahara.ac.jp>

---
+++ Assets/Script/WeponScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public class WeponScript : MonoBehaviour
+{
+    [Header("CoolDown Settings")]
+    //武器ごとのクールタイム。
+    public float coolTime = 0.86f;
+
+    [SerializeField]
+    protected PlayerStateScript playerState;
+    enum MoveState
+    {
+        Forward,
+        Return
+    }
+    MoveState moveState;
+
+    public float speed = 7.0f;
+    public float maxDistance = 10.0f;
+    public float returnSpeed = 6.4f;
+    public float catchDistance = 0.4f;
+
+    [SerializeField]
+    protected Transform player;
+    protected Vector2 startPos;
+    protected Vector2 forwardDir;
+
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeponAction();
+    }
+
+    public virtual void WeponAction()
+    {
+        if (player == null) return;
+        Vector2 currentPos = transform.position;
+
+        if (moveState == MoveState.Forward)
+        {
+            currentPos += forwardDir * speed * Time.deltaTime;
+
+            if (Vector2.Distance(startPos, transform.position) >= maxDistance)
+            {
+                moveState = MoveState.Return;
+            }
+        }
+        else
+        {
+            Vector2 dir = ((Vector2)player.position - currentPos).normalized;
+            currentPos += dir * returnSpeed * Time.deltaTime;
+
+            if (Vector2.Distance(transform.position, player.position) <= catchDistance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+        transform.position = currentPos;
+    }
+
+    public void Initialize()
+    {
+        startPos = transform.position;
+        forwardDir = player.transform.right;
+        moveState = MoveState.Forward;
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            //TODO:Enemyにダメージを与えるところエネミー担当の人はここを
+            //var enemyとかにしてGetComponentしてエネミーがnullじゃないときに
+            //ダメージを与えるようにしてください。
+        }
+    }
+}
